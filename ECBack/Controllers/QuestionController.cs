d37e142607778a17c105eec7e82bc7b23cbfe693
Wautumn <2283149823@qq.com>
2018-07-24 18:36:47 +0800

    Question
    Question controller

--- ECBack/Controllers/QuestionController.cs
+++ ECBack/Controllers/QuestionController.cs
@@ -17,27 +17,52 @@ namespace ECBack.Controllers
     public class QuestionController : ApiController
     {
         private OracleDbContext db = new OracleDbContext();
+        private const int PageDataNumber = 10;
 
         //GET:api/QuestionEntities
+        [Route("api/Questions")]
+        [HttpGet]
         public IQueryable<Question> GetQuestions()
         {
             return db.Questions;
         }
 
+        //根据当前商品id得到当前商品的所有问题，前端传过来的是SKU1的id还有当前页数。。，然后返回所有这个商品的问题记录
         //GET:api/Questions/5
+    
         [ResponseType(typeof(Question))]
-        public async Task<IHttpActionResult>GetQuestion(int id)
+        [Route("api/Questions/{id:int}")]
+        [HttpGet]
+        public async Task<IHttpActionResult> GetQuestion(int id,[FromUri]int pn)
         {
-            Question question = await db.Questions.FindAsync(id);
-            if(question==null)
+            if (!ModelState.IsValid)
             {
-                return NotFound();
+                return BadRequest(ModelState);
             }
-            return Ok(question);
+
+
+            IQueryable<Question> questions;
+            var ques = await db.GoodEntities.FindAsync(id);
+            db.Entry(ques).Collection(c => c.Questions).Load();          
+            questions = ques.Questions.AsQueryable();
+            foreach (var que in ques.Questions)
+            {
+                await db.Entry(que).Collection(a => a.Replies).LoadAsync();
+            }
+            var QuestionEntities = await questions.Skip((pn - 1) * PageDataNumber).Take(PageDataNumber).ToListAsync();
+            return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                PageCount=QuestionEntities.Count()/PageDataNumber,
+                QuestionEntities
+                
+            }));
+           
         }
 
-        //PUT:api/Questions/5
+        //PUT:api/Questions/5.......其实我感觉put没必要啊，问题提出了不能修改了
         [ResponseType(typeof(void))]
+        [Route("api/Questions/{id:int}")]
+        [HttpPut]
         public async Task<IHttpActionResult> PutQuestion(int id,[FromBody]Question question)
         {
             if(!ModelState.IsValid)
@@ -67,30 +92,32 @@ namespace ECBack.Controllers
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-       
+        //添加问题的,需要有当前商品的SKU1和新添加的问题的详细信息,
+        //Post:api/Questions
         [Route("api/Questions")]
         [HttpPost]
         public HttpResponseMessage PostQuestion([FromBody] JObject obj )
         {
-           
-            int Display_id = int.Parse(obj["Display_id"].ToString());
-            string QuestionDetail = obj["qu_detail"].ToString();
+          
+            int Goodid = int.Parse(obj["GoodID"].ToString());
+            string QuestionDetail = obj["QuestionDetail"].ToString();
+            Question question = new Question();
+            question.Detail = QuestionDetail;
+            
             HttpResponseMessage response;
 
-            GoodEntity Display = db.GoodEntities.Find(Display_id);
+            //找到这个商品的东东
+            GoodEntity goodEntity = db.GoodEntities.Find(Goodid);
+            goodEntity.Questions.Add(question);
            
-            if (Display == null)
+           
+            if (goodEntity == null)
             {
-                response = Request.CreateResponse(HttpStatusCode.NotFound, "the DisplayEntity not exists");
+                response = Request.CreateResponse(HttpStatusCode.NotFound, "the GoodEntity not exists");
             }
             else
             {
-               
-                Question question = new Question();
-                question.Detail = QuestionDetail;
-                question.QuestionID = Display_id;
-                db.Questions.Add(question);
-              
+                db.Questions.Add(question);                     
                 response = Request.CreateResponse(HttpStatusCode.OK, "Created");
             }
             db.SaveChanges();
@@ -98,23 +125,25 @@ namespace ECBack.Controllers
         }
 
        
-
+        //删除问题的，我觉得可能用不到就先写在这里,需要有问题的id
         //Delete:api/Questions/5
         [ResponseType(typeof(Question))]
+        [Route("api/Questions/{id:int}")]
+        [HttpDelete]
         public async Task<IHttpActionResult> DeleteAddress(int id)
         {
             Question question = await db.Questions.FindAsync(id);
             if (question == null)
+            {
                 return NotFound();
+            }
             db.Questions.Remove(question);
+            
             await db.SaveChangesAsync();
 
             return Ok(question);
             
         }
-
-
-
 
         protected override void Dispose(bool disposing)
         {
@@ -130,7 +159,7 @@ namespace ECBack.Controllers
             return db.Questions.Count(e => e.QuestionID == id)>0;
         }
 
-        private int QuestionAmount(int id)//
+        private int QuestionAmount(int id)//问题数，不知道前面要不要，先放这里
         {
             return db.Questions.Count(e => e.QuestionID == id);
         }
